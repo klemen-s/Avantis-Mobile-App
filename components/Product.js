054612b50ba417e1f9f2086e0eb4832b90c84f3c
Klemen Slatnar <klemenslatnar@Klemens-MacBook-Air.local>
2023-12-21 14:09:18 +0100

    Men's and Women's products page added

--- components/Product.js
+++ components/Product.js
@@ -1,11 +1,32 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
-export default function Product() {
+export default function Product({ productName, imageUrl, id, price }) {
   return (
     <View
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      style={{
+        width: "100%",
+        height: 500,
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 35,
+      }}
     >
-      <Text>Product</Text>
+      <Image
+        style={{ width: "100%", height: "100%", resizeMode: "contain" }}
+        source={{ uri: imageUrl }}
+      />
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 10,
+          marginTop: 7
+        }}
+      >
+        <Text style={{ fontSize: 17, fontWeight: "400" }}>{productName}</Text>
+        <Text style={{ fontSize: 16, marginTop: 5 }}>{price}</Text>
+      </View>
     </View>
   );
 }
